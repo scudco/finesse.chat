10b3656ad168474a03fa876a8f248137fddda6f9
scudco <3806+scudco@users.noreply.github.com>
2026-03-02 11:11:26 -0500

    add simple markdown

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ApplicationRecord
   validates :content, :author, presence: true
+  validates :content, length: { maximum: 2000 }
 
   after_create_commit -> { broadcast_append_to "chat" }
 end
