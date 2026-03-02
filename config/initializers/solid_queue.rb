cf149686d75d63e0d8eaebd7e99d5f05554a5ffb
scudco <3806+scudco@users.noreply.github.com>
2026-03-02 16:34:52 -0500

    Add bot storm

--- config/initializers/solid_queue.rb
+++ config/initializers/solid_queue.rb
@@ -1,8 +1,3 @@
 Rails.application.config.after_initialize do
-  SolidQueue.constants.each do |const|
-    klass = SolidQueue.const_get(const)
-    klass.strict_loading_by_default = false if klass.is_a?(Class) && klass < ActiveRecord::Base
-  rescue NameError
-    next
-  end
+  SolidQueue::Record.strict_loading_by_default = false
 end
