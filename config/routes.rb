1a1f982f7f52ff646601afd24f4f3ca11b5883be
scudco <3806+scudco@users.noreply.github.com>
2026-03-02 18:36:45 -0500

    styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :messages do
+  resources :messages, only: %i[ index create update destroy ] do
     collection do
       get :older
       get :newer
@@ -7,15 +7,8 @@ Rails.application.routes.draw do
     end
   end
   resource :session, only: [:destroy]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   root "messages#index"
 
