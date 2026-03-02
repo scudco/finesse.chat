e99a9b88ffe275b8416728625ed0496935aee1ad
scudco <3806+scudco@users.noreply.github.com>
2026-03-02 13:01:01 -0500

    fix some issues around username

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,7 @@ class Message < ApplicationRecord
   validates :content, :author, presence: true
   validates :content, length: { maximum: 2000 }
 
-  after_create_commit -> { broadcast_append_to "chat" }
+  after_create_commit  -> { broadcast_append_to "chat" }
+  after_update_commit  -> { broadcast_replace_to "chat" }
+  after_destroy_commit -> { broadcast_remove_to "chat" }
 end
