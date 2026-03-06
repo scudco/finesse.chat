073f882751429643a1362825570890841bd93e70
scudco <3806+scudco@users.noreply.github.com>
2026-03-05 22:43:55 -0500

    fix system test

--- test/system/messages_test.rb
+++ test/system/messages_test.rb
@@ -1,13 +1,16 @@
 require "application_system_test_case"
 
 class MessagesTest < ApplicationSystemTestCase
-  test "visiting the chat shows the page" do
+  def setup
     visit messages_url
+    click_button "Start chatting"
+  end
+
+  test "visiting the chat shows the page" do
     assert_selector "h1", text: ChannelName::NAMES.first
   end
 
   test "sending a message displays it in the chat" do
-    visit messages_url
     fill_in "message[content]", with: "hello system test"
     click_button "Send"
     assert_text "hello system test"
@@ -16,6 +19,7 @@ class MessagesTest < ApplicationSystemTestCase
   test "empty state is shown when there are no messages" do
     Message.delete_all
     visit messages_url
+    click_button "Start chatting"
     assert_selector ".chat-empty-state"
   end
 end
