36f3fe806a07a2655b6e9f1578e5659676c48bcd
scudco <3806+scudco@users.noreply.github.com>
2026-03-07 12:18:35 -0500

    fix username rotation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 class SessionsController < ApplicationController
-  def destroy
-    reset_session
-    redirect_to root_path
+  def update
+    session[:username] = ApplicationController::WORDS.sample(2).map(&:capitalize).join
+    color = helpers.author_color(current_username)
+    render turbo_stream: turbo_stream.replace("current-username",
+      helpers.tag.strong(current_username, id: "current-username", style: "color: #{color}"))
   end
 end
