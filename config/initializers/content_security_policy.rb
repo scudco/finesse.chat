989d64eb89bfe83899eba5e33af7b870d10031d1
scudco <3806+scudco@users.noreply.github.com>
2026-03-11 09:53:20 -0400

    Allow cloudflare

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
     policy.font_src    :self, :data, "https://fonts.gstatic.com"
     policy.img_src     :self, :data
     policy.object_src  :none
-    policy.script_src  :self, "'unsafe-inline'"
+    policy.script_src  :self, "'unsafe-inline'", "https://static.cloudflareinsights.com"
     policy.style_src   :self, "'unsafe-inline'", "https://fonts.googleapis.com"
     policy.connect_src :self, ENV.fetch("SSE_HOST", "http://localhost:4000")
   end
